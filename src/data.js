290f5251cce181f1e3925578e3110eb21e7d4f29
Nam Nguyen <hnamnguyencse@gmail.com>
2020-06-05 21:01:06 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,530 @@
+var mainjson = {
+	"dress":[
+		{
+			"name":"HeBe Dress",
+			"price":"400.000",
+			"path":"_product-1.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Cube Dress",
+			"price":"450.000",
+			"path":"_product-2.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lotus Dress",
+			"price":"350.000",
+			"path":"_product-3.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Tuli Dress",
+			"price":"400.000",
+			"path":"_product-4.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amy Dress",
+			"price":"380.000",
+			"path":"_product-5.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chill Dress",
+			"price":"350.000",
+			"path":"_product-6.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"BiBi Dress",
+			"price":"400.000",
+			"path":"_product-7.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Rose Dress",
+			"price":"450.000",
+			"path":"_product-20.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lavender Dress",
+			"price":"300.000",
+			"path":"_product-9.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"HM Dress",
+			"price":"380.000",
+			"path":"_product-10.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pixie Dress",
+			"price":"400.000",
+			"path":"_product-11.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Kimi Dress",
+			"price":"380.000",
+			"path":"_product-12.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Queen Dress",
+			"price":"400.000",
+			"path":"_product-13.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Tilly Dress",
+			"price":"350.000",
+			"path":"_product-14.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lera Dress",
+			"price":"400.000",
+			"path":"_product-15.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Aki Dress",
+			"price":"380.000",
+			"path":"_product-16.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lou Dress",
+			"price":"350.000",
+			"path":"_product-17.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pony Dress",
+			"price":"450.000",
+			"path":"_product-18.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lena Dress",
+			"price":"350.000",
+			"path":"_product-19.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Momo Dress",
+			"price":"380.000",
+			"path":"_product-20.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chie Dress",
+			"price":"350.000",
+			"path":"_product-21.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Moriko Dress",
+			"price":"380.000",
+			"path":"_product-22.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Meiko Dress",
+			"price":"400.000",
+			"path":"_product-23.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amber Dress",
+			"price":"300.000",
+			"path":"_product-24.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Yumi Dress",
+			"price":"380.000",
+			"path":"_product-25.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Saki Dress",
+			"price":"380.000",
+			"path":"_product-26.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Kristy Dress",
+			"price":"480.000",
+			"path":"_product-27.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Jane Dress",
+			"price":"400.000",
+			"path":"_product-28.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Calee Dress",
+			"price":"350.000",
+			"path":"_product-29.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Berry Dress",
+			"price":"400.000",
+			"path":"_product-30.jpg",
+			"description":"Size: S M L"
+		},
+	],
+	"top":[
+	    {
+			"name":"HeBe Top",
+			"price":"200.000",
+			"path":"top-1.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Cube Top",
+			"price":"250.000",
+			"path":"top-2.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lotus Top",
+			"price":"350.000",
+			"path":"top-3.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Tuli Top",
+			"price":"300.000",
+			"path":"top-4.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amy Top",
+			"price":"380.000",
+			"path":"top-5.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chill Top",
+			"price":"350.000",
+			"path":"top-6.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"BiBi Top",
+			"price":"400.000",
+			"path":"top-7.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Rose Top",
+			"price":"350.000",
+			"path":"top-8.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lavender Top",
+			"price":"300.000",
+			"path":"top-9.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"HM Top",
+			"price":"380.000",
+			"path":"top-10.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pixie Top",
+			"price":"300.000",
+			"path":"top-11.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Kimi Top",
+			"price":"280.000",
+			"path":"top-12.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Queen Top",
+			"price":"400.000",
+			"path":"top-13.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Tilly Top",
+			"price":"350.000",
+			"path":"top-14.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lera Top",
+			"price":"400.000",
+			"path":"top-15.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Aki Top",
+			"price":"280.000",
+			"path":"top-16.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lou Top",
+			"price":"350.000",
+			"path":"top-17.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pony Top",
+			"price":"350.000",
+			"path":"top-18.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lena Top",
+			"price":"350.000",
+			"path":"top-19.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Momo Top",
+			"price":"280.000",
+			"path":"top-20.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chie Top",
+			"price":"350.000",
+			"path":"top-21.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Moriko Top",
+			"price":"280.000",
+			"path":"top-22.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Meiko Top",
+			"price":"200.000",
+			"path":"top-23.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amber Top",
+			"price":"300.000",
+			"path":"top-24.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Yumi Top",
+			"price":"280.000",
+			"path":"top-25.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Saki Top",
+			"price":"280.000",
+			"path":"top-26.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Kristy Top",
+			"price":"280.000",
+			"path":"top-27.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Jane Top",
+			"price":"300.000",
+			"path":"top-28.jpg",
+			"description":"Size: S M L"
+		},
+	],
+	"skirt":[
+		{
+			"name":"HeBe Skirt",
+			"price":"200.000",
+			"path":"skirt-1.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Cube Skirt",
+			"price":"250.000",
+			"path":"skirt-2.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lotus Skirt",
+			"price":"350.000",
+			"path":"skirt-3.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Tuli Skirt",
+			"price":"300.000",
+			"path":"skirt-4.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amy Skirt",
+			"price":"380.000",
+			"path":"skirt-5.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chill Skirt",
+			"price":"350.000",
+			"path":"skirt-6.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Rose Skirt",
+			"price":"350.000",
+			"path":"skirt-8.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lavender Skirt",
+			"price":"300.000",
+			"path":"skirt-9.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"HM Skirt",
+			"price":"380.000",
+			"path":"skirt-10.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pixie Skirt",
+			"price":"300.000",
+			"path":"skirt-11.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Kimi Top",
+			"price":"280.000",
+			"path":"top-12.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Queen Top",
+			"price":"400.000",
+			"path":"top-13.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Tilly Top",
+			"price":"350.000",
+			"path":"top-14.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lera Skirt",
+			"price":"400.000",
+			"path":"skirt-15.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Aki Skirt",
+			"price":"280.000",
+			"path":"skirt-16.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lou Skirt",
+			"price":"350.000",
+			"path":"skirt-17.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Pony Skirt",
+			"price":"350.000",
+			"path":"skirt-18.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Lena Skirt",
+			"price":"350.000",
+			"path":"skirt-19.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Momo Skirt",
+			"price":"280.000",
+			"path":"skirt-20.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Chie Skirt",
+			"price":"350.000",
+			"path":"skirt-21.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Moriko Skirt",
+			"price":"280.000",
+			"path":"skirt-22.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Meiko Skirt",
+			"price":"200.000",
+			"path":"skirt-23.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Amber Skirt",
+			"price":"300.000",
+			"path":"skirt-24.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Yumi Skirt",
+			"price":"280.000",
+			"path":"skirt-25.jpg",
+			"description":"Size: S M "
+		},
+		{
+			"name":"Saki Skirt",
+			"price":"280.000",
+			"path":"skirt-26.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Kristy Skirt",
+			"price":"280.000",
+			"path":"skirt-27.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Jane Skirt",
+			"price":"300.000",
+			"path":"skirt-28.jpg",
+			"description":"Size: S M L"
+		},
+		{
+			"name":"Calee Skirt",
+			"price":"250.000",
+			"path":"skirt-29.jpg",
+			"description":"Size: Freesize"
+		},
+		{
+			"name":"Berry Skirt",
+			"price":"300.000",
+			"path":"skirt-30.jpg",
+			"description":"Size: S M L"
+		},
+]
+}
